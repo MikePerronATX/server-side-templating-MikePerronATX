795f5b32f92526dd3772a2984df1e60b179cc87f
Michael Perron <michael.perron@g.austincc.edu>
2021-02-06 18:06:22 -0600

    reworked the nav menu so that they links are calling the home and a page called 'form'

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $footer = file_get_contents('templates/footer.html');
 // this will be used to send the page title into the page
 // you can add more things to send if you like
 $header_data = ["pagetitle" => "Code03 Michael Perron", "logo" => "Code03", "home" => HOME,
-                "home2" => HOME2, "home3" => HOME3];
+                "home2" => FORM];
 
 // notice this holds mixed numeric and string data,
 // you can do this in a loosly typed language like PHP
