20781ec9e108483be16c254f1b847b1027f990cd
Michael Perron <michael.perron@g.austincc.edu>
2021-03-04 06:50:59 -0600

    added processing code to the point where I have trimmed, strpped, and substring data

--- email.php
+++ email.php
@@ -5,12 +5,15 @@
 Coding 05
 Purpose: display.
  */
-require_once 'mustache/mustache/src/Mustache/Autoloader.php';
-Mustache_Autoloader::register();
 
-$mustache = new Mustache_Engine;
+require_once 'mustache/mustache/src/Mustache/Autoloader.php';
+
 
 function successpage() {
+
+    Mustache_Autoloader::register();
+
+    $mustache = new Mustache_Engine;
 
     $header = file_get_contents('templates/header.html');
     $body = file_get_contents('templates/successpage.html');
@@ -26,9 +29,16 @@ function successpage() {
     $footer_data = [
         "localtime" => date('l jS \of F Y h:i:s A'),
         "footertitle" => "Success Page"];
+
+    echo $mustache->render($header, $header_data) . PHP_EOL;
+    echo $mustache->render($body, $body_data) . PHP_EOL;
+    echo $mustache->render($footer, $footer_data) . PHP_EOL;
 }
 
 function failurepage() {
+    Mustache_Autoloader::register();
+
+    $mustache = new Mustache_Engine;
 
     $header = file_get_contents('templates/header.html');
     $body = file_get_contents('templates/failurepage.html');
@@ -44,26 +54,14 @@ function failurepage() {
     $footer_data = [
         "localtime" => date('l jS \of F Y h:i:s A'),
         "footertitle" => "Failure Page"];
+    echo $mustache->render($header, $header_data) . PHP_EOL;
+    echo $mustache->render($body, $body_data) . PHP_EOL;
+    echo $mustache->render($footer, $footer_data) . PHP_EOL;
+
 }
 
 function main() {
-
-   /* ********************************************
-    * TODO (erase this comment when you are done)
-    * ********************************************
-    * these two lines are for debugging purposes to get started.
-    * erase them both when you are ready to complete this processing
-    */
     
-
-    // var_dump($_POST);
-    // exit();
-    
-
-
-
-    /* This will test to make sure we have a non-empty $_POST from
-     * the form submission. */
     if (!empty($_POST)) {
         /* ********************************************
          * TODO (erase this comment when you are done)
@@ -77,34 +75,85 @@ function main() {
            https://www.php.net/manual/en/function.filter-var.php
          * 
          */
+        
+/* * *********************************************
+* STEP 1: INPUT: Do NOT process, just get the data.
+* Do not delete this comment,
+* ********************************************** */
+
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $from = $_POST['from'];
 
 
-        /* The cleaning routines above may leave any variable empty. If we
-         * find an empty variable, we stop processing because that means
-         * someone tried to send us something malicious and/or incorrect. */
-        if (!empty($name) && !empty($from) && !empty($subject) && !empty($message)) {
+/* * ******************************************************
+* STEP 2: VALIDATION: Always clean your input first!!!!
+* Do NOT process, only CLEAN and VALIDATE.
+* Do not delete this comment.
+* ****************************************************** */
 
-            /* this forms the correct email headers to send an email */
-            $headers = "From: $from\r\n";
-            $headers .= "Reply-To: $from\r\n";
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+        $name = trim($name);
+        $subject = trim($subject);
+        $message = trim($message);
+        $from = trim($from);
+        
+        $name = strip_tags($name);
+        $subject = strip_tags($subject);
+        $message = strip_tags($message);
+        $from = strip_tags($from);
 
-            /* Now attempt to send the email. This uses a REAL email function
-             * and will send an email. Make sure you only sned it to yourself.
-             * server, you would use just "mail" instead of "mymail" and
-             * it will be sent normally. Read about the PHP mail() function
-             * https://www.php.net/manual/en/function.mail.php
-             * then it's up to you to fill out the paramters correctly.
-             */
-            if (mail(/****fill out the paramters here****/)) {
-                successpage();
-            } else {
-                failurepage();
-            }
-        } else {
-            failurepage();
-        }
+        $nameLen = strlen($name);
+        $subjectLen = strlen($subject);
+        $messageLen = strlen($message);
+        $fromLen = strlen($from);
+      
+        $name = substr($name, $nameLen, 64);
+        $subject = substr($subject, $subjectLen, 64);
+        $message = substr($message, $messageLen, 1000);
+        $from = substr($from, $fromLen, 64);
+
+        successpage();
+        echo $nameLen;
+        echo $subjectLen;
+        echo $messageLen;
+        echo $fromLen;
+
+/* * *************************************************************************
+* STEP 3 and 4: PROCESSING and OUTPUT: Notice this code only executes
+* if you have valid data from steps 1 and 2. Your code must always have
+* a saftey feature similar to this.
+* Do not delete this comment.
+* ************************************************************************ */
+        
+        
+        
+        // /* The cleaning routines above may leave any variable empty. If we
+        //  * find an empty variable, we stop processing because that means
+        //  * someone tried to send us something malicious and/or incorrect. */
+        // if (!empty($name) && !empty($from) && !empty($subject) && !empty($message)) {
+
+        //     /* this forms the correct email headers to send an email */
+        //     $headers = "From: $from\r\n";
+        //     $headers .= "Reply-To: $from\r\n";
+        //     $headers .= "MIME-Version: 1.0\r\n";
+        //     $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+
+        //     /* Now attempt to send the email. This uses a REAL email function
+        //      * and will send an email. Make sure you only sned it to yourself.
+        //      * server, you would use just "mail" instead of "mymail" and
+        //      * it will be sent normally. Read about the PHP mail() function
+        //      * https://www.php.net/manual/en/function.mail.php
+        //      * then it's up to you to fill out the paramters correctly.
+        //      */
+        //     if (mail(/****fill out the paramters here****/)) {
+        //         successpage();
+        //     } else {
+        //         failurepage();
+        //     }
+        // } else {
+        //     failurepage();
+        // }
     } else {
         failurepage();
     }
