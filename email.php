b16b2ec1b4330ba782879cd2e20111646ea2bb3f
Michael Perron <michael.perron@g.austincc.edu>
2021-03-06 09:12:29 -0600

    experimented with code to cause form to pass security verifaications and submit email.

--- email.php
+++ email.php
@@ -62,19 +62,8 @@ function failurepage() {
 
 function main() {
     
-    if (!empty($_POST)) {
-        /* ********************************************
-         * TODO (erase this comment when you are done)
-         * ********************************************
-         * Do your validation and cleaning here. You need to extract FOUR
-         * things from the $_POST array...
-           $name --> trim it, strip HTML tags, and sub-string it to 64
-           $subject --> trim it, strip HTML tags, and sub-string it to 64
-           $message --> trim it, strip HTML tags, and sub-string it to 1000
-           $from --> look up and use the PHP filter_var() with FILTER_VALIDATE_EMAIL
-           https://www.php.net/manual/en/function.filter-var.php
-         * 
-         */
+    if (!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message'])
+                && !empty($_POST['from'])) {
         
 /* * *********************************************
 * STEP 1: INPUT: Do NOT process, just get the data.
@@ -93,67 +82,64 @@ function main() {
 * Do not delete this comment.
 * ****************************************************** */
 
-        $name = trim($name);
-        $subject = trim($subject);
-        $message = trim($message);
-        $from = trim($from);
-        
-        $name = strip_tags($name);
-        $subject = strip_tags($subject);
-        $message = strip_tags($message);
-        $from = strip_tags($from);
+    if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+        echo("$from is a valid email address");
+    } else {
+    echo("$from is not a valid email address");
+    }
 
-        $nameLen = strlen($name);
-        $subjectLen = strlen($subject);
-        $messageLen = strlen($message);
-        $fromLen = strlen($from);
-      
-        $name = substr($name, $nameLen, 64);
-        $subject = substr($subject, $subjectLen, 64);
-        $message = substr($message, $messageLen, 1000);
-        $from = substr($from, $fromLen, 64);
+    
 
-        successpage();
-        echo $nameLen;
-        echo $subjectLen;
-        echo $messageLen;
-        echo $fromLen;
+    $name = trim($name);
+    $subject = trim($subject);
+    $message = trim($message);
+    
+    $name = strip_tags($name);
+    $subject = strip_tags($subject);
+    $message = strip_tags($message);
+    
+    $name = substr($name, $nameLen, 64);
+    $subject = substr($subject, $subjectLen, 64);
+    $message = substr($message, $messageLen, 1000);
 
-/* * *************************************************************************
-* STEP 3 and 4: PROCESSING and OUTPUT: Notice this code only executes
-* if you have valid data from steps 1 and 2. Your code must always have
-* a saftey feature similar to this.
-* Do not delete this comment.
-* ************************************************************************ */
-        
-        
-        
-        // /* The cleaning routines above may leave any variable empty. If we
-        //  * find an empty variable, we stop processing because that means
-        //  * someone tried to send us something malicious and/or incorrect. */
-        // if (!empty($name) && !empty($from) && !empty($subject) && !empty($message)) {
+    $nameLen = strlen($name);
+    $subjectLen = strlen($subject);
+    $messageLen = strlen($message);
+    
+    
+        /* The cleaning routines above may leave any variable empty. If we
+         * find an empty variable, we stop processing because that means
+         * someone tried to send us something malicious and/or incorrect. */
+        if (!empty($name) && !empty($from) && !empty($subject) && !empty($message)) {
+            
+            /* this forms the correct email headers to send an email */
+            $headers = "From: $from\r\n";
+            $headers = "Name: $name\r\n";
+            $headers .= "Reply-To: $from\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 
-        //     /* this forms the correct email headers to send an email */
-        //     $headers = "From: $from\r\n";
-        //     $headers .= "Reply-To: $from\r\n";
-        //     $headers .= "MIME-Version: 1.0\r\n";
-        //     $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-
-        //     /* Now attempt to send the email. This uses a REAL email function
-        //      * and will send an email. Make sure you only sned it to yourself.
-        //      * server, you would use just "mail" instead of "mymail" and
-        //      * it will be sent normally. Read about the PHP mail() function
-        //      * https://www.php.net/manual/en/function.mail.php
-        //      * then it's up to you to fill out the paramters correctly.
-        //      */
-        //     if (mail(/****fill out the paramters here****/)) {
-        //         successpage();
-        //     } else {
-        //         failurepage();
-        //     }
-        // } else {
-        //     failurepage();
-        // }
+            /* Now attempt to send the email. This uses a REAL email function
+             * and will send an email. Make sure you only sned it to yourself.
+             * server, you would use just "mail" instead of "mymail" and
+             * it will be sent normally. Read about the PHP mail() function
+             * https://www.php.net/manual/en/function.mail.php
+             * then it's up to you to fill out the paramters correctly.
+             */
+            
+            $to = "mikeperron.atx@gmail.com";
+            
+            if (mail($to, $subject, $message, $headers)) {
+                $time = time();
+                print "Script Ran $time";
+                successpage();
+                
+            } else {
+                failurepage();
+            }
+        } else {
+            failurepage();
+        }
     } else {
         failurepage();
     }
