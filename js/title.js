32c10494a98d265b77dbee40654cd2314c39bf62
Michael Perron <michael.perron@g.austincc.edu>
2021-03-07 11:24:09 -0600

    added title.js to my form.html page. added email.js to contact.html page

--- js/title.js
+++ js/title.js
@@ -18,11 +18,6 @@ function clearForm() {
     $('#msg').html('<br>'); // minor violation of concerns, but okay for now
 }
 
-function validEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
 function validate() {
     //empty error message
     var errorMessage = "";
