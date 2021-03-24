b865d03d9069e5770c8ae0f29475dbfdf6506869
Michael Perron <michael.perron@g.austincc.edu>
2021-03-23 21:52:42 -0500

    changed main comment to Coding 06. removed unneeded comments. Removed mustache code. Making process.php more modular by putting the processing code in to a main function.

--- process.php
+++ process.php
@@ -1,122 +1,69 @@
 <?php
 /*
 Name: Michael Perron
-Coding 05
+Coding 06
 This file receives input from the user, validates it,
 processes it, and sends it to result.html to be displated.
 */
+function main() {
 
-require_once 'mustache/mustache/src/Mustache/Autoloader.php';
-Mustache_Autoloader::register();
+    if (!empty($_POST)){
 
-$mustache = new Mustache_Engine;
+        $title = $_POST['title'];
+        $drink = $_POST['drink'];
+        $pet = $_POST['pet'];
+        $ficPlace = $_POST['ficPlace'];
+        $rlPlace = $_POST['rlPlace'];
+        $email = $_POST['email'];
+        $remail = $_POST['remail'];
+            
+        $title = trim($title);
+        $drink = trim($drink);
+        $pet = trim($pet);
+        $ficPlace = trim($ficPlace);
+        $rlPlace = trim($rlPlace);
+        $email = trim($email);
+        $remail = trim($remail);
 
-$header = file_get_contents('templates/header.html');
-$body = file_get_contents('templates/result.html');
-$footer = file_get_contents('templates/footer.html');
+        $title = strip_tags($title);
+        $drink = strip_tags($drink);
+        $pet = strip_tags($pet);
+        $ficPlace = strip_tags($ficPlace);
+        $rlPlace = strip_tags($rlPlace);
+        $email = strip_tags($email);
+        $remail = strip_tags($remail);
 
-$header_data = ["pagetitle" => "Code03 Michael Perron",
-                "logo" => "Code03", 
-                "home" => HOME,
-                "form" => FORM,
-                "contact" => CONTACT];
+        $title = substr($title, 0 , 64);
+        $drink = substr($drink, 0 , 64);
+        $pet = substr($pet, 0 , 64);
+        $ficPlace = substr($ficPlace, 0 , 64);
+        $rlPlace = substr($rlPlace, 0 , 64);
+        $email = substr($email, 0 , 64);
+        $remail = substr($remail, 0 , 64);
 
-$footer_data = [
-    "localtime" => date('l jS \of F Y h:i:s A'),
-    "footertitle" => "Result Page"];
+        $titleLen = strlen($title);
+        $drinkLen = strlen($drink);
+        $petLen = strlen($pet);
+        $ficLen = strlen($ficPlace);
+        $realLen = strlen($rlPlace);
+        $emailLen = strlen($email);
+        $remailLen = strlen($remail);
+        $sentence = "You are " . $title . " " . $drink . " " . $pet . " of ". $ficPlace . " and " . $rlPlace;
+        $sentenceLength = strlen($sentence);
 
-/* * *********************************************
-* STEP 1: INPUT: Do NOT process, just get the data.
-* Do not delete this comment,
-* ********************************************** */
-if (!empty($_POST['title']) && !empty($_POST['drink']) && !empty($_POST['pet'])
-                && !empty($_POST['ficPlace']) && !empty($_POST['rlPlace'])
-                && !empty($_POST['email']) && !empty($_POST['remail'])) {
+        if($sentenceLength > 30){
+            $heckcute = "That’s a heck of a title!​";
+        }
+        if($sentenceLength < 30){
+            $heckcute = "That’s a cute little title.";
+        }
 
-    $title = $_POST['title'];
-    $drink = $_POST['drink'];
-    $pet = $_POST['pet'];
-    $ficPlace = $_POST['ficPlace'];
-    $rlPlace = $_POST['rlPlace'];
-    $email = $_POST['email'];
-    $remail = $_POST['remail'];
-
-    /* * ******************************************************
-    * STEP 2: VALIDATION: Always clean your input first!!!!
-    * Do NOT process, only CLEAN and VALIDATE.
-    * Do not delete this comment.
-    * ****************************************************** */            
-    $title = trim($title);
-    $drink = trim($drink);
-    $pet = trim($pet);
-    $ficPlace = trim($ficPlace);
-    $rlPlace = trim($rlPlace);
-    $email = trim($email);
-    $remail = trim($remail);
-
-    $title = strip_tags($title);
-    $drink = strip_tags($drink);
-    $pet = strip_tags($pet);
-    $ficPlace = strip_tags($ficPlace);
-    $rlPlace = strip_tags($rlPlace);
-    $email = strip_tags($email);
-    $remail = strip_tags($remail);
-
-    $title = substr($title, 0 , 64);
-    $drink = substr($drink, 0 , 64);
-    $pet = substr($pet, 0 , 64);
-    $ficPlace = substr($ficPlace, 0 , 64);
-    $rlPlace = substr($rlPlace, 0 , 64);
-    $email = substr($email, 0 , 64);
-    $remail = substr($remail, 0 , 64);
-
-    $titleLen = strlen($title);
-    $drinkLen = strlen($drink);
-    $petLen = strlen($pet);
-    $ficLen = strlen($ficPlace);
-    $realLen = strlen($rlPlace);
-    $emailLen = strlen($email);
-    $remailLen = strlen($remail);
-    $sentence = "You are " . $title . " " . $drink . " " . $pet . " of ". $ficPlace . " and " . $rlPlace;
-    $sentenceLength = strlen($sentence);
-
-    if($sentenceLength > 30){
-        $heckcute = "That’s a heck of a title!​";
+        if (!empty($title) && !empty($drink) && !empty($pet) && !empty($ficPlace) && !empty($rlPlace)) {
     }
-    if($sentenceLength < 30){
-        $heckcute = "That’s a cute little title.";
-    }
-
-    if (!empty($title) && !empty($drink) && !empty($pet) && !empty($ficPlace) && !empty($rlPlace)) {
-    /* * *************************************************************************
-    * STEP 3 and 4: PROCESSING and OUTPUT: Notice this code only executes
-    * if you have valid data from steps 1 and 2. Your code must always have
-    * a saftey feature similar to this.
-    * Do not delete this comment.
-    * ************************************************************************ */                
-    $body_data = [
-        "result" => $sentence,
-        "titlelen" => $titleLen,
-        "drinklen" => $drinkLen,
-        "petlen" => $petLen,
-        "ficlen" => $ficLen,
-        "reallen" => $realLen,
-        "emaillen" => $emailLen,
-        "remaillen" => $remailLen,
-        "sentlen" => $sentenceLength,
-        "empty" => "d-none",
-        "heckcute" => $heckcute,];
-    }
-}
-else {
-    $title = "";
-    $drink = "";
-    $pet = "";
-    $ficPlace = "";
-    $rlPlace = "";
     
-    $body_data = ["full" =>  "d-none"];
+    else {
+        echo "error";
+    }
 }
-echo $mustache->render($header, $header_data) . PHP_EOL;
-echo $mustache->render($body, $body_data) . PHP_EOL;
-echo $mustache->render($footer, $footer_data) . PHP_EOL;
+// this kicks off the script
+main();
